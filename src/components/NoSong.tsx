687fcca7754508599cb165da458fe9afd3efdda9
holoflash <mikepalacemusic@gmail.com>
2023-02-28 17:02:16 +0100

    Fix type issue for NoSong

--- src/components/NoSong.tsx
+++ src/components/NoSong.tsx
@@ -1,9 +1,5 @@
 function NoSong({ term }: { term: string }) {
-    return (
-        term && (
-            <p className="no-song-message">No song titled "{term}" exists!</p>
-        )
-    );
+    return <p className="no-song-message">No song titled "{term}" exists!</p>;
 }
 
 export default NoSong;
